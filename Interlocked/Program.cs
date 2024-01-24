4f736d6e376e17e5565ca68b0bd2e36452dde3ff
Denis Vieira dos Anjos <denis.vieira@authoritypartners.com>
2024-01-24 12:46:18 -0300

    Interlocked and locked

---
+++ Interlocked/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Interlocked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Op
+    {
+        int value;
+
+        public int Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        public void Increment()
+        {
+            //value++;
+            System.Threading.Interlocked.Increment(ref value);
+        }
+
+        public void Set(int newValue)
+        {
+            // this.value = value;
+            System.Threading.Interlocked.Exchange(ref value, newValue);
+        }
+
+        public void CompareAndChange(int compare, int newValue)
+        {
+            //if (value == compare)
+            //{
+            //    value = newValue;
+            //}
+
+            System.Threading.Interlocked.CompareExchange(ref value, newValue, compare);
+        }
+    }
+}
---
+++ Locks/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Locks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => { Run(counter); });
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Name = $"Thread {i}";
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            Console.WriteLine("Contador: " + counter.Value);
+        }
+
+        private static void Run(Counter counter)
+        {
+            counter.Increment();
+        }
+    }
+
+    class Counter
+    {
+        private readonly object sync = new object();
+
+        public int Value { get; private set; }
+
+        public void Increment()
+        {
+            //lock(sync)
+            //{
+            //    Console.WriteLine(Thread.CurrentThread.Name);
+            //    Value++;
+            //}
+
+            var lockTaken = false;
+            try
+            {
+                Monitor.Enter(sync, ref lockTaken);
+                Value++;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(sync);
+                }
+            }
+        }
+    }
+}
