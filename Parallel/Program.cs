6942e8a902a1c7da575cce146cdaf5612163239f
Denis Vieira dos Anjos <denisvieira.dev@gmail.com>
2024-01-26 18:03:45 -0300

    Parallel

---
+++ Parallel/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //Parallel.For(0, 100, i =>
+        //{
+
+        //    Console.WriteLine(i);
+        //});
+
+        //Parallel.For(0, 100, (i, status) =>
+        //{
+        //    if(i > 10)
+        //    {
+        //        //status.Break();
+        //        status.Stop();
+        //    }
+
+        //    Console.WriteLine(i);
+        //});
+
+        string[] array = { "A", "B", "C", "D", "E" };
+
+        Parallel.ForEach(array, i => { Console.WriteLine(i); });
+
+        Parallel.Invoke(Processar1, Processar2);
+
+        Console.WriteLine();
+    }
+
+    static void Processar1()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("-");
+        }
+    }
+
+    static void Processar2()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write("+");
+        }
+    }
+
+}
